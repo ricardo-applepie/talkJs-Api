392af6f4484fda188ea010f5e4eec2c6bc011c64
ricardo-applepie <mbancho12@gmail.com>
2021-09-13 00:05:13 +0200

    completed app and embedded chat system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Talk from 'talkjs';
+import { useEffect} from 'react';
+import React from "react"
 function App() {
+  const talkjsContainer = React.createRef();
+  useEffect(() => {
+    const currentUser = {
+      id: 1,
+      name: "rick",
+      email: "rick@gmail.com",
+      photoUrl: "myusrl",
+      welcomeMessage: 'Hey there! How are you? :-)',
+      role: 'default',
+    }
+ 
+    Talk.ready.then(() => {
+      var me = new Talk.User({
+        id: currentUser.id,
+        name: currentUser.name,
+        email: currentUser.email,
+        photoUrl: currentUser.photo,
+        welcomeMessage: 'Hey there! How are you? :-)',
+        role: 'default',
+      });
+      var other = new Talk.User({
+        id: '654321',
+        name: 'Sebastian',
+        email: 'Sebastian@example.com',
+        photoUrl: 'https://demo.talkjs.com/img/sebastian.jpg',
+        welcomeMessage: 'Hey, how can I help?',
+        role: 'default',
+      });
+
+      let test = window.talkSession = new Talk.Session({
+        appId: 't7AkyA9y',
+        me: me,
+      });
+    
+
+      var conversation = window.talkSession.getOrCreateConversation(
+        Talk.oneOnOneId(me, other)
+      );
+
+  
+      conversation.setParticipant(me);
+      conversation.setParticipant(other);
+      console.log(conversation);
+      var inbox = window.talkSession.createInbox({ selected: conversation });
+      console.log(inbox)
+
+
+
+      
+      inbox.mount(talkjsContainer.current);
+
+    });
+
+  }, []);
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>testing</p>
+      <div ref={talkjsContainer }></div>
     </div>
   );
 }
